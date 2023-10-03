ff1f5a6007ffac53031153acede20a4d71ad5c9a
Samuel Muli <samuelmuli95@gmail.com>
2023-10-03 15:00:21 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -3,9 +3,47 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  const errorModal = document.getElementById('modal');
+  errorModal.classList.add('hidden');
 
+  let isLiked = false
 
+  const heartElement = document.querySelector('.like-glyph');
+  heartElement.addEventListener('click', () => {
+    mimicServerCall()
+      .then(() => {
+        if (isLiked) {
+          // Change heart back to empty and remove activated-heart class
+          heartElement.innerHTML = EMPTY_HEART;
+          heartElement.classList.remove('activated-heart');
+          isLiked = false;
+        } else {
+          // Change heart to full and add activated-heart class on success
+          heartElement.innerHTML = FULL_HEART;
+          heartElement.classList.add('activated-heart');
+          isLiked = true;
+        }
+      })
+      .catch((error) => {
+        // Display error modal on failure
+        const modalMessage = document.querySelector('#modal-message');
+        modalMessage.textContent = error;
+        errorModal.classList.remove('hidden');
+        setTimeout(() => {
+          errorModal.classList.add('hidden');
+        }, 3000);
+      });
+  });
 
+  // Handling full heart clicks
+  heartElement.addEventListener('click', () => {
+    if(heartElement.innerHTML === FULL_HEART){
+      heartElement.innerHTML = EMPTY_HEART
+      heartElement.classList.remove('activated-heart')
+    } 
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
